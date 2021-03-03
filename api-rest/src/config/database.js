614244b00779e813661bc557d7b50a8967a874c5
Daniel dos Santos Araujo <denenewton@icloud.com>
2021-03-03 01:38:43 -0300

    add

--- api-rest/src/config/database.js
+++ api-rest/src/config/database.js
@@ -1,16 +1,11 @@
 require('dotenv').config();
 
 module.exports = {
-  /* SQLite
-  dialect: 'sqlite',
-  storage: './db.sqlite',*/
-
-  /* /* MySQL / MariaDB */
   dialect: 'mysql',
   host: process.env.DATABASE_HOST,
   port: process.env.DATABASE_PORT,
   username: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
+  password: process.env.BATABASE_PASSWORD,
   database: process.env.DATABASE,
 
   define: {
@@ -18,12 +13,12 @@ module.exports = {
     underscored: true,
     underscoredAll: true,
     createdAt: 'created_at',
-    updatedAt: 'updated_at',
+    updatedAt: 'updated_at'
   },
 
   dialectOptions: {
-    timezone: '-03:00',
+    timezone: '-03:00'
   },
 
-  timezone: '-03:00',
+  timezone: '-03:00'
 };
--- api-rest/src/database/migrations/20210302035148-escola.js
+++ api-rest/src/database/migrations/20210302035148-escola.js
@@ -7,44 +7,44 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
         autoIncrement: true,
-        primaryKey: true,
+        primaryKey: true
       },
-      nome:{
+      nome: {
         type: Sequelize.STRING,
-        allowNull: false,
+        allowNull: false
       },
-      sobrenome:{
+      sobrenome: {
         type: Sequelize.STRING,
-        allowNull: false,
+        allowNull: false
       },
-      email:{
+      email: {
         type: Sequelize.STRING,
-        allowNull: false,
+        allowNull: false
       },
       idade: {
         type: Sequelize.INTEGER,
-        allowNull: false,
+        allowNull: false
       },
       peso: {
         type: Sequelize.FLOAT,
-        allowNull: false,
+        allowNull: false
       },
       altura: {
         type: Sequelize.FLOAT,
-        allowNull: false,
+        allowNull: false
       },
       created_at: {
         type: Sequelize.DATE,
-        allowNull: false,
+        allowNull: false
       },
       updated_at: {
         type: Sequelize.DATE,
-        allowNull: false,
+        allowNull: false
       }
-     });
+    });
   },
 
-  down: async (queryInterface) => {
+  down: async queryInterface => {
     await queryInterface.dropTable('alunos');
   }
 };
